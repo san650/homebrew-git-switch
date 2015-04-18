e7b47a0d35bf4b4d8c82969ea40e82fb8bedff9d
Santiago Ferreira <san650@gmail.com>
2015-04-18 19:42:35 -0300

    WIP Update to version 1.1.0

--- git-switch.rb
+++ git-switch.rb
@@ -1,21 +1,16 @@
 class GitSwitch < Formula
   homepage "https://github.com/san650/git-switch"
-  url "https://github.com/san650/git-switch/archive/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "f1a1a3272f68799ca902ac8f3fb5454914503cd925c9731d4b4fbb1f61a52c6b"
+  url "https://github.com/san650/git-switch/archive/v1.1.0.tar.gz"
+  version "1.1.0"
+  #sha256 "f1a1a3272f68799ca902ac8f3fb5454914503cd925c9731d4b4fbb1f61a52c6b"
+
+  head "https://github.com/san650/git-switch.git"
 
   def install
-    bin.mkpath
-
-    mv "git-switch.rb", "git-switch"
-    mv "git-switch.man", "git-switch.1"
-
-    bin.install "git-switch"
-    man1.install "git-switch.1"
-    bash_completion.install "git-switch-completion.bash"
+    # no need to do anything
   end
 
   test do
-    assert_equal "git-switch version 1.0.0", `#{bin}/git-switch --version`.strip
+    assert_equal "git-switch version 1.1.0", `#{bin}/git-switch --version`.strip
   end
 end
